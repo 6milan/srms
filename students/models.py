5435980be909b797e0116ce78d36f13f77cadd97
Gideon <iamdegideons@gmail.com>
2023-05-11 00:08:26 +0100

    Changes on Student models.py file

--- students/models.py
+++ students/models.py
@@ -4,28 +4,25 @@ from django.urls import reverse
 from datetime import date
 
 
-
 # Create your models here.
 
 class Student(models.Model):
-	select_gender = (
-		('Male', 'Male'),
-		('Female', 'Female'),
-		('Other', 'Other'),
-	)
- 
-	
-	student_name = models.CharField(max_length=100)
-	student_roll = models.IntegerField(unique=True)
-	student_email = models.EmailField()
-	student_gender = models.CharField(max_length=8, choices=select_gender)
-	student_class = models.ForeignKey(StudentClass, on_delete=models.CASCADE)
-	student_date_of_birth = models.DateTimeField(auto_now_add=True)
-	student_reg = models.DateField(auto_now_add=True, auto_now=False)
-	
-	def get_absolute_url(self):
-		return reverse('students:student_create')
-	
-	def __str__(self):
-		return self.student_name
-    
+    select_gender = (
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Other', 'Other'),
+    )
+
+    student_name = models.CharField(max_length=100)
+    student_roll = models.IntegerField(unique=True)
+    student_email = models.EmailField()
+    student_gender = models.CharField(max_length=8, choices=select_gender)
+    student_class = models.ForeignKey(StudentClass, on_delete=models.CASCADE)
+    student_date_of_birth = models.DateTimeField(auto_now_add=False)
+    student_reg = models.DateField(auto_now_add=True, auto_now=False)
+
+    def get_absolute_url(self):
+        return reverse('students:student_create')
+
+    def __str__(self):
+        return self.student_name
