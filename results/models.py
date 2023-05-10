f13cf480b2f7a06ea338e3b2b844b4f27b59db65
Gideon <iamdegideons@gmail.com>
2023-05-10 22:58:15 +0100

    Second Commit

--- results/models.py
+++ results/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from subjects.models import StudentClass
+from students.models import Student
+from django.urls import reverse
+from django.contrib.postgres.fields import JSONField
+
+from django.db import migrations, models
+
+try:
+    from django.db.models import JSONField
+except ImportError:
+    from django.contrib.postgres.fields import JSONField
+
+
+class Migration(migrations.Migration):
+    dependencies = [('testapp', '0001_initial')]
+
+    operations = [
+        migrations.CreateModel(
+            name='MyModel',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('stuff', JSONField()),
+            ],
+        ),
+    ]
+
 
 # Create your models here.
+
+class DeclareResult(models.Model):
+    select_class = models.ForeignKey(StudentClass, on_delete=models.CASCADE)
+    select_student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    marks = JSONField(blank=True)
+
+    def get_absolute_url(self):
+        return reverse('results:declare_result')
+
+    def __str__(self):
+        return "%s Section-%s" % (self.select_class.class_name, self.select_class.section)
--- srms/settings.py
+++ srms/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,6 +31,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'dashboard',
+    'student_classes',
+    'subjects',
+    'students',
+    'results',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +60,7 @@ ROOT_URLCONF = 'srms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -109,14 +115,20 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
-STATIC_URL = 'static/'
-
+LOGOUT_REDIRECT_URL = 'home'
+LOGIN_URL = 'home'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
